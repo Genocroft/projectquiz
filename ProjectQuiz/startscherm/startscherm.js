1fe22aea6eb3d3e76f427a5f99be7055852908af
Genocraft <gynocraft@gmail.com>
2025-09-18 11:30:35 +0200

    Delete ProjectQuiz directory

--- ProjectQuiz/startscherm/startscherm.js
+++
@@ -1,9 +0,0 @@
-function startquiz() {
-  const nameInput = document.querySelector('.inputname').value;
-  if (nameInput.trim() === "") {
-    alert("Please enter your name!");
-    return;
-  }
-  localStorage.setItem('spelernaam', nameInput);
-  window.location.href = "../themascherm/themascherm.html";
-} 
